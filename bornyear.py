ac02599557585a804c622f374791d90d38ed7511
Alexey Makarov <axel.makarov@gmail.com>
2019-10-09 21:03:54 +0300

    Add files via upload

---
+++ bornyear.py
@@ -0,0 +1,12 @@
+# Pushkin: may 26 1799
+
+p_year = 1799
+p_day = 26
+
+print(type(p_year))
+
+year = int(input("Год рождения А.С. Пушкина: "))
+
+if year != p_year:
+    print("Неверный год рождения")
+print("Верно!")
